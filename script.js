0e1c4a1a29744001111714b229dafcb95c81670b
marroccuandrea <marroccuandrea@yahoo.it>
2024-02-19 15:15:36 +0100

    [add] prompt firstname, surname & color

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('esercizio generatore di password');
+
+//1.
+const firstname = prompt('Inserisci il nome');
+console.log(firstname);
+
+//2.
+const lastname = prompt('Inserisci il cognome');
+console.log(lastname);
+
+//3.
+const color = prompt('Qual è il tuo colore preferito?')
+console.log(color);
